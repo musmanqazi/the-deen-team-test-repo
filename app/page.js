db6265d78021c1e0a9828d45b741006470b9421d
musmanqazi <usmanqazi8@gmail.com>
2024-08-04 10:27:10 -0700

    fixed project

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Box, Typography} from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box display="flex" flexDirection="column" justifyContent="center" width="100vw" height="100vh" gap={2}>
+      <Typography variant="h3" textAlign="center" alignItems="center">Hello World</Typography>
+      <Typography variant="h5" textAlign="center" alignItems="center">Usman Qazi</Typography>
+      <Typography variant="h5" textAlign="center" alignItems="center">Ahad Ahmad</Typography>
+      
+      <Typography variant="h5" textAlign="center" alignItems="center">Ibrahim Israr</Typography>
+    </Box>
+  );
+}
